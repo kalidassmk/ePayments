8327d782810bd190903ed4dfe758dbf2121ab1df
kamahalingam <kamahalingam@ap.equinix.com>
2017-10-13 12:10:11 +0800

    first commit

--- src/main/java/com/epayments/EPaymentsApplication.java
+++ src/main/java/com/epayments/EPaymentsApplication.java
@@ -2,11 +2,13 @@ package com.epayments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * The type E payments application.
  */
 @SpringBootApplication
+@EnableAsync
 public class EPaymentsApplication {
     /**
      * The entry point of application.
